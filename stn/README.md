4432e5da5adce1f3a29030fe63029468ef876b0a
d1onys1us <13951458+d1onys1us@users.noreply.github.com>
2024-01-17 16:51:54 -0500

    ship shitty v1 of tool

--- stn/README.md
+++ stn/README.md
@@ -11,10 +11,39 @@
 
 # CLI
 
-- `stn setup`: set up the configuration details of your stn, declaratively
+- `stn config`: set up the configuration details of your stn, declaratively
 - `stn help`: view all stn commands
 - `stn start`: start up your stn
 - `stn stop`: stop your stn
+- `stn delete`: delete your stn
 - `stn upgrade`: upgrade your stn instance
-- `stn logs`: view all logs
-- `stn logs (el|cl)`: view either el or cl logs
+- `stn logs (proposer|prover|zkevm|client)`: view all logs
+
+# `stnd config` flow
+
+Hello! What would you like to config?
+
+1. Enable proposer
+2. Enable prover
+3. Update L1 node RPC
+
+## L1 node RPC
+
+- Enter http: [prefilled with docker local]
+- Enter ws: [prefilled with docker local]
+
+### Proposer
+
+- Enter your proposer private key:
+- Enter a prover endpoint: [prefilled with local, link to market]
+
+### Prover
+
+- Enter your prover private key:
+- Enter SGX Raiko host: [prefilled with local, link to market]
+
+# Problems
+
+1. simple-taiko-node doesn't include running SGX prover
+2. new prover market page is needed
+3. hard to understand how to run a prover in SGX mode, or ZK mode, etc.
--- stn/src/main.rs
+++ stn/src/main.rs
@@ -1,15 +1,128 @@
-use dotenv::dotenv;
-use std::env;
+use clap::{Parser, Subcommand};
+use std::process::{Command, Stdio};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Up,
+    Down,
+    Terminate,
+    Upgrade,
+}
 
 fn main() {
-    // load from .env
-    let result = dotenv();
+    let cli = Cli::parse();
 
-    match result {
-        Ok(_) => println!("Loaded .env file successfully"),
-        Err(e) => println!("Error loading .env file: {}", e),
+    match &cli.command {
+        Commands::Up => {
+            up();
+        }
+        Commands::Down => {
+            down();
+        }
+        Commands::Terminate => {
+            terminate();
+        }
+        Commands::Upgrade => {
+            upgrade();
+        }
+    }
+}
+
+fn up() {
+    println!("up");
+}
+
+fn down() {
+    println!("down");
+}
+
+fn terminate() {
+    println!("terminate");
+}
+
+fn upgrade() {
+    // Check if Docker daemon is running
+    let docker_daemon_running = check_docker_daemon();
+
+    if !docker_daemon_running {
+        stn_log("Docker daemon is not running. Please start Docker!");
+        return;
     }
 
-    let greeting = env::var("GREETING").expect("GREETING not found");
-    println!("{}", greeting);
+    // Pull latest simple-taiko-node from GitHub
+    let mut git_pull = Command::new("git")
+        .arg("pull")
+        .arg("origin")
+        .arg("main")
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .spawn()
+        .expect("Failed to execute git pull command.");
+
+    let git_pull_status = git_pull
+        .wait()
+        .expect("Failed to wait for git pull to complete.");
+
+    if git_pull_status.success() {
+        stn_log("Git pull successful.");
+    } else {
+        stn_log("Git pull failed.");
+    }
+
+    // Update docker images
+    let mut docker_compose_pull = Command::new("docker-compose")
+        .arg("pull")
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .spawn()
+        .expect("Failed to execute docker-compose pull command.");
+
+    let docker_compose_pull_status = docker_compose_pull
+        .wait()
+        .expect("Failed to wait for docker-compose pull to complete.");
+
+    if docker_compose_pull_status.success() {
+        stn_log("Docker compose pull successful.");
+    } else {
+        stn_log("Docker compose pull failed.");
+    }
+
+    // Execute a script with bash: in ./scripts/util/update-env.sh
+    let mut update_env = Command::new("bash")
+        .arg("./script/util/update-env.sh")
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .spawn()
+        .expect("Failed to execute update-env.sh script.");
+
+    let update_env_status = update_env
+        .wait()
+        .expect("Failed to wait for update-env.sh to complete.");
+
+    if update_env_status.success() {
+        stn_log("update-env.sh script successful.");
+    } else {
+        stn_log("update-env.sh script failed.");
+    }
+}
+
+fn check_docker_daemon() -> bool {
+    let docker_version = Command::new("docker")
+        .arg("version")
+        .output()
+        .expect("Failed to execute docker version command.");
+
+    docker_version.status.success()
+}
+
+fn stn_log(msg: &str) {
+    println!("stn_log: {}", msg);
 }
