a003e8cc70ba4ef8406e007c176c1d0d1c400017
d1onys1us <13951458+d1onys1us@users.noreply.github.com>
2024-01-24 01:38:01 -0500

    fix: add check for installation before status, fix some logging (#46)

--- crates/stn/src/main.rs
+++ crates/stn/src/main.rs
@@ -186,15 +186,17 @@ fn config(taiko_node_dir: &Path) {
 }
 
 fn up(taiko_node_dir: &Path) {
-    docker::execute_docker_command(&["compose", "up", "-d"], taiko_node_dir)
-        .expect("Failed to execute docker compose up -d command");
-    utils::stn_log("simple-taiko-node successfully started");
+    match docker::execute_docker_command(&["compose", "up", "-d"], taiko_node_dir) {
+        Ok(_) => utils::stn_log("simple-taiko-node successfully started"),
+        Err(_) => panic!("Failed to execute docker compose up -d command"),
+    }
 }
 
 fn down(taiko_node_dir: &Path) {
-    docker::execute_docker_command(&["compose", "down"], taiko_node_dir)
-        .expect("Failed to execute docker compose down command");
-    utils::stn_log("simple-taiko-node successfully stopped");
+    match docker::execute_docker_command(&["compose", "down"], taiko_node_dir) {
+        Ok(_) => utils::stn_log("simple-taiko-node successfully stopped"),
+        Err(_) => panic!("Failed to execute docker compose down command"),
+    }
 }
 
 fn upgrade(taiko_node_dir: &Path) {
@@ -272,6 +274,11 @@ fn logs(log_type: &LogsSubcommands, taiko_node_dir: &Path) {
 }
 
 fn status(taiko_node_dir: &Path) {
+    // Check taiko node is installed first
+    if !taiko_node_dir.exists() {
+        utils::stn_log("simple-taiko-node is not installed.");
+        return;
+    }
     docker::execute_docker_command(&["ps"], taiko_node_dir)
         .expect("Failed to execute docker ps command")
 }
