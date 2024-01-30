e5d5c7f039549decb34db33685b6f91d3563921d
d1onys1us <13951458+d1onys1us@users.noreply.github.com>
2024-01-29 22:31:13 -0500

    feat: extend logs with proposer, and add stats command

--- crates/stn/src/main.rs
+++ crates/stn/src/main.rs
@@ -41,6 +41,8 @@ enum Commands {
     Logs(Logs),
     /// Status of your Taiko node
     Status,
+    /// System usage stats of your Taiko node
+    Stats,
 }
 
 #[derive(Parser)]
@@ -63,6 +65,8 @@ enum LogsSubcommands {
     Execution,
     /// Shows driver logs
     Driver,
+    /// Show proposer logs
+    Proposer,
 }
 
 #[derive(Subcommand)]
@@ -116,6 +120,9 @@ async fn main() {
         }
         Commands::Status => {
             status(&taiko_node_dir).await;
+        }
+        Commands::Stats => {
+            stats(&taiko_node_dir);
         }
     }
 }
@@ -495,6 +502,9 @@ fn logs(log_type: &LogsSubcommands, taiko_node_dir: &Path) {
         LogsSubcommands::Driver => {
             args.push("taiko_client_driver");
         }
+        LogsSubcommands::Proposer => {
+            args.push("taiko_client_proposer");
+        }
     }
 
     match docker::execute_docker_command(&args, taiko_node_dir) {
@@ -532,6 +542,22 @@ async fn status(taiko_node_dir: &Path) {
         }
         Err(error) => {
             eprintln!("Error checking syncing status: {}", error);
+        }
+    }
+}
+
+fn stats(taiko_node_dir: &Path) {
+    if !taiko_node_dir.exists() {
+        utils::stn_log("simple-taiko-node is not installed.");
+        return;
+    }
+
+    match docker::execute_docker_command(&["stats"], taiko_node_dir) {
+        Ok(msg) => {
+            utils::stn_log(&msg);
+        }
+        Err(e) => {
+            eprintln!("{}", e);
         }
     }
 }
