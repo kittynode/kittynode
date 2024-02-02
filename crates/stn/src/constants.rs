d94c9d224f6150868ae9f1fcb00afc95eeb33dc0
d1onys1us <13951458+d1onys1us@users.noreply.github.com>
2024-02-02 14:30:14 -0500

    feat: implement update checker (#117)

--- crates/stn/src/constants.rs
+++ crates/stn/src/constants.rs
@@ -1,5 +1,8 @@
 pub const SIMPLE_TAIKO_NODE_REPO_URL: &str = "https://github.com/taikoxyz/simple-taiko-node";
-pub const TAIKO_NODE_PATH: &str = ".stn/taiko-node";
+pub const STN_PATH: &str = ".stn";
+pub const TAIKO_NODE_DIRECTORY_NAME: &str = "taiko-node";
 pub const DEFAULT_NETWORK_TIMEOUT: u64 = 10;
 pub const KATLA_RPC_URL: &str = "https://rpc.katla.taiko.xyz";
 pub const DEFAULT_PROVER_URL: &str = "http://taiko-a6-prover.zkpool.io:9876";
+pub const GITHUB_REPO_USERNAME: &str = "d1onys1us";
+pub const GITHUB_REPO_NAME: &str = "stn";
---
+++ crates/stn/src/stn_config.rs
@@ -0,0 +1,47 @@
+use serde::{Deserialize, Serialize};
+use std::{
+    fs,
+    io::{self, ErrorKind},
+    path::Path,
+};
+
+#[derive(Serialize, Deserialize)]
+pub struct StnConfig {
+    pub last_update_check: u64,
+    pub latest_version: Option<String>,
+}
+
+impl StnConfig {
+    pub fn load(stn_config_dir: &Path) -> Result<Self, io::Error> {
+        let config_path = stn_config_dir.join("stn_config.toml");
+        let content = match fs::read_to_string(config_path) {
+            Ok(content) => content,
+            Err(_) => {
+                return Ok(StnConfig {
+                    last_update_check: 0,
+                    latest_version: None,
+                })
+            } // If file not found, return default config
+        };
+        let config = toml::from_str(&content)?;
+        Ok(config)
+    }
+
+    pub fn save(&self, stn_config_dir: &Path) -> Result<(), io::Error> {
+        fs::create_dir_all(stn_config_dir)?;
+
+        let config_path = stn_config_dir.join("stn_config.toml");
+        let content = toml::to_string(self).map_err(|err| {
+            io::Error::new(
+                ErrorKind::InvalidData,
+                format!("Failed to serialize config: {}", err),
+            )
+        })?;
+        fs::write(config_path, content.as_bytes()).map_err(|err| {
+            io::Error::new(
+                ErrorKind::Other,
+                format!("Failed to write config to file: {}", err),
+            )
+        })
+    }
+}
