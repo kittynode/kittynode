fabbc0a2d270cb153554db42f986417b3a0efa5d
d1onys1us <13951458+d1onys1us@users.noreply.github.com>
2024-09-15 06:43:39 -0400

    chore: make clippy happy

--- crates/kittynode_gui/src-tauri/src/lib.rs
+++ crates/kittynode_gui/src-tauri/src/lib.rs
@@ -4,26 +4,29 @@ use tracing::info;
 #[tauri::command]
 fn check_running_nodes() -> Result<i32, String> {
     info!("Checking running nodes");
-    Ok(kittynode_core::check_running_nodes()
+    let num_nodes = kittynode_core::check_running_nodes()
         .wrap_err("Error checking running nodes")
-        .map_err(|e| e.to_string())?)
+        .map_err(|e| e.to_string())?;
+    Ok(num_nodes)
 }
 
 #[tauri::command]
 fn install_node() -> Result<(), String> {
     info!("Installing node");
-    Ok(kittynode_core::install()
+    kittynode_core::install()
         .wrap_err("Error installing node")
-        .map_err(|e| e.to_string())?)
+        .map_err(|e| e.to_string())?;
+    Ok(())
 }
 
 #[tauri::command]
 async fn check_docker_version() -> Result<(), String> {
     info!("Checking docker version");
-    Ok(kittynode_core::check_docker_version()
+    kittynode_core::check_docker_version()
         .await
         .wrap_err("Error checking docker version")
-        .map_err(|e| e.to_string())?)
+        .map_err(|e| e.to_string())?;
+    Ok(())
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
