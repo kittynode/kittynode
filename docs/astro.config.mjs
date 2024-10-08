11b93af9bfa4635eee6963220a573f6357efac3b
d1onys1us <13951458+d1onys1us@users.noreply.github.com>
2024-10-08 15:25:18 -0400

    add github pages workflow

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://kittynode.io",
   integrations: [
     starlight({
       title: "Kittynode Docs",
