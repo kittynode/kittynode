43f7c5f83cfd0cdb0fde2668f3009c0035bc9ad3
dionysuz <13951458+dionysuzx@users.noreply.github.com>
2024-11-04 14:48:41 +0700

    chore(repo): bump deps (#403)

--- crates/kittynode-core/src/system_info.rs
+++ crates/kittynode-core/src/system_info.rs
@@ -1,6 +1,6 @@
 use eyre::Result;
 use serde::{Deserialize, Serialize};
-use sysinfo::{CpuExt, DiskExt, System, SystemExt};
+use sysinfo::{Disks, System};
 
 #[derive(Serialize, Deserialize)]
 pub struct SystemInfo {
@@ -13,7 +13,7 @@ pub fn get_system_info() -> Result<SystemInfo> {
     let mut system = System::new_all();
     system.refresh_all();
 
-    // Get processor details
+    // Get processor details and convert to gigahertz
     let cpu_name = system
         .cpus()
         .first()
@@ -21,11 +21,13 @@ pub fn get_system_info() -> Result<SystemInfo> {
         .unwrap_or_else(|| "Unknown".to_string());
 
     let cpu_cores = system.physical_core_count().unwrap_or(1);
+
     let cpu_frequency = system
         .cpus()
         .first()
-        .map(|cpu| cpu.frequency() as f64 / 1000.0) // Convert MHz to GHz
+        .map(|cpu| cpu.frequency() as f64 / 1000.0)
         .unwrap_or(0.0);
+
     let cpu_architecture = match std::env::consts::ARCH {
         "x86_64" => "x86_64",
         "aarch64" => "ARM",
@@ -38,20 +40,21 @@ pub fn get_system_info() -> Result<SystemInfo> {
     );
 
     // Fetch total RAM in bytes and convert to gigabytes
-    let total_ram = system.total_memory() as f64 / (1024.0 * 1024.0 * 1024.0); // Convert from bytes to GB
+    let total_ram = system.total_memory() as f64 / (1024.0 * 1024.0 * 1024.0);
+
     let memory = format!("{:.2} GB RAM", total_ram);
 
-    // Get total and available disk space
-    let total_disk_space = system
-        .disks()
+    // Get total and available disk space and convert to gigabytes
+    let disks = Disks::new_with_refreshed_list();
+
+    let total_disk_space = disks
         .iter()
-        .map(|d| d.total_space() as f64 / 1_024_000_000.0) // Convert to GB
+        .map(|d| d.total_space() as f64 / 1_024_000_000.0)
         .sum::<f64>();
 
-    let available_disk_space = system
-        .disks()
+    let available_disk_space = disks
         .iter()
-        .map(|d| d.available_space() as f64 / 1_024_000_000.0) // Convert to GB
+        .map(|d| d.available_space() as f64 / 1_024_000_000.0)
         .sum::<f64>();
 
     let storage = format!(
