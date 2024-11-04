49ec38f46e9f8df2053f89f9d7aac96e7cec6727
dionysuz <13951458+dionysuzx@users.noreply.github.com>
2024-11-04 14:24:59 +0700

    just init shadcn

--- apps/kittynode-gui/kittynode-frontend/src/routes/+layout.svelte
+++ apps/kittynode-gui/kittynode-frontend/src/routes/+layout.svelte
@@ -1,16 +1,17 @@
 <script lang="ts">
-import "@picocss/pico/css/pico.purple.min.css";
-import { onMount } from "svelte";
-import { windowShownStore } from "../stores/windowShown.svelte.ts";
-import { initializedStore } from "../stores/initialized.svelte";
-import Welcome from "./Welcome.svelte";
-import Header from "./Header.svelte";
-
-const { children } = $props();
-
-onMount(async () => {
-  await windowShownStore.show();
-});
+  import "../app.css";
+  import "@picocss/pico/css/pico.purple.min.css";
+  import { onMount } from "svelte";
+  import { windowShownStore } from "../stores/windowShown.svelte.ts";
+  import { initializedStore } from "../stores/initialized.svelte";
+  import Welcome from "./Welcome.svelte";
+  import Header from "./Header.svelte";
+  
+  const { children } = $props();
+  
+  onMount(async () => {
+    await windowShownStore.show();
+  });
 </script>
 
 {#if !initializedStore.initialized}
