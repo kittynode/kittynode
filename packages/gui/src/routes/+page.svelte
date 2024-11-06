359c0923d3b36722a628e540a94029269bd11564
dionysuz <13951458+dionysuzx@users.noreply.github.com>
2024-11-06 22:28:12 +0700

    fixup

--- packages/gui/src/routes/+page.svelte
+++ packages/gui/src/routes/+page.svelte
@@ -1,72 +1,72 @@
 <script lang="ts">
-  import { onMount } from "svelte";
-  import { invoke } from "@tauri-apps/api/core";
-  import type { Package } from "$lib/types";
-  import Dashboard from "./Dashboard.svelte";
-  import { Button } from "$lib/components/ui/button";
-  import * as Card from "$lib/components/ui/card/index.js";
-  import { platform } from "@tauri-apps/plugin-os";
+import { onMount } from "svelte";
+import { invoke } from "@tauri-apps/api/core";
+import type { Package } from "$lib/types";
+import Dashboard from "./Dashboard.svelte";
+import { Button } from "$lib/components/ui/button";
+import * as Card from "$lib/components/ui/card/index.js";
+import { platform } from "@tauri-apps/plugin-os";
 
-  let packages: { [name: string]: Package } = $state({});
-  let isDockerRunning: boolean | null = $state(null);
-  let installedPackages: Package[] = $state([]);
-  let installLoading: string | null = $state(null); // Track which package is being installed
-  let deleteLoading: string | null = $state(null); // Track which package is being deleted
-  let ready = $state(false); // Track when state checks are complete
-  let currentPlatform = $state("");
+let packages: { [name: string]: Package } = $state({});
+let isDockerRunning: boolean | null = $state(null);
+let installedPackages: Package[] = $state([]);
+let installLoading: string | null = $state(null); // Track which package is being installed
+let deleteLoading: string | null = $state(null); // Track which package is being deleted
+let ready = $state(false); // Track when state checks are complete
+let currentPlatform = $state("");
 
-  async function loadPackages() {
-    try {
-      packages = await invoke("get_packages");
-      if (isDockerRunning) {
-        installedPackages = await invoke("get_installed_packages");
-      }
-    } catch (error) {
-      alert(`Failed to load packages: ${error}`);
-      console.error(error);
-    } finally {
-      ready = true;
+async function loadPackages() {
+  try {
+    packages = await invoke("get_packages");
+    if (isDockerRunning) {
+      installedPackages = await invoke("get_installed_packages");
     }
+  } catch (error) {
+    alert(`Failed to load packages: ${error}`);
+    console.error(error);
+  } finally {
+    ready = true;
   }
+}
 
-  async function installPackage(name: string) {
-    installLoading = name;
-    try {
-      await invoke("install_package", { name });
-      await loadPackages();
-      alert(`Successfully installed ${name}.`);
-    } catch (error) {
-      alert(`Failed to install ${name}.`);
-      console.error(error);
-    } finally {
-      installLoading = null;
-    }
-  }
-
-  async function deletePackage(name: string, includeImages: boolean) {
-    deleteLoading = name;
-    try {
-      await invoke("delete_package", { name, includeImages });
-      await loadPackages();
-      alert(`Successfully deleted ${name}.`);
-    } catch (error) {
-      alert(`Failed to delete ${name}.`);
-      console.error(error);
-    } finally {
-      deleteLoading = null;
-    }
-  }
-
-  async function checkDocker() {
-    isDockerRunning =
-      currentPlatform === "ios" ? true : await invoke("is_docker_running");
-  }
-
-  onMount(async () => {
-    currentPlatform = platform();
-    await checkDocker();
+async function installPackage(name: string) {
+  installLoading = name;
+  try {
+    await invoke("install_package", { name });
     await loadPackages();
-  });
+    alert(`Successfully installed ${name}.`);
+  } catch (error) {
+    alert(`Failed to install ${name}.`);
+    console.error(error);
+  } finally {
+    installLoading = null;
+  }
+}
+
+async function deletePackage(name: string, includeImages: boolean) {
+  deleteLoading = name;
+  try {
+    await invoke("delete_package", { name, includeImages });
+    await loadPackages();
+    alert(`Successfully deleted ${name}.`);
+  } catch (error) {
+    alert(`Failed to delete ${name}.`);
+    console.error(error);
+  } finally {
+    deleteLoading = null;
+  }
+}
+
+async function checkDocker() {
+  isDockerRunning =
+    currentPlatform === "ios" ? true : await invoke("is_docker_running");
+}
+
+onMount(async () => {
+  currentPlatform = platform();
+  await checkDocker();
+  await loadPackages();
+});
 </script>
 
 <h2>Dashboard:</h2>
--- packages/gui/src/routes/Welcome.svelte
+++ packages/gui/src/routes/Welcome.svelte
@@ -1,24 +1,24 @@
 <script lang="ts">
-  import { initializedStore } from "../stores/initialized.svelte";
-  import { goto } from "$app/navigation";
-  import { platform } from "@tauri-apps/plugin-os";
-  import { onMount } from "svelte";
-  import { Button } from "$lib/components/ui/button";
+import { initializedStore } from "../stores/initialized.svelte";
+import { goto } from "$app/navigation";
+import { platform } from "@tauri-apps/plugin-os";
+import { onMount } from "svelte";
+import { Button } from "$lib/components/ui/button";
 
-  let currentPlatform = $state("");
+let currentPlatform = $state("");
 
-  async function initKittynode() {
-    if (currentPlatform === "ios") {
-      await initializedStore.cheatInitialize();
-    } else {
-      await initializedStore.initialize();
-    }
-    await goto("/");
+async function initKittynode() {
+  if (currentPlatform === "ios") {
+    await initializedStore.cheatInitialize();
+  } else {
+    await initializedStore.initialize();
   }
+  await goto("/");
+}
 
-  onMount(async () => {
-    currentPlatform = platform();
-  });
+onMount(async () => {
+  currentPlatform = platform();
+});
 </script>
 
 <main class="flex flex-col justify-center items-center h-full text-center p-4">
