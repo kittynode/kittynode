ce89d1b4370a61dcf55272d01f381bcc43b06c2b
dionysuz <13951458+dionysuzx@users.noreply.github.com>
2024-11-18 10:04:01 +0700

    feat: add gui updater

--- packages/gui/src-tauri/src/lib.rs
+++ packages/gui/src-tauri/src/lib.rs
@@ -265,6 +265,7 @@ async fn init_kittynode(server_url: String) -> Result<(), String> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() -> Result<()> {
     tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_os::init())
