afaea94ef42a9a5602f8b0484a49cdd58fac50b0
dionysuz <13951458+dionysuzx@users.noreply.github.com>
2024-11-26 13:11:14 +0700

    chore: testing stuff (#471)

--- packages/cli/tests/integration.rs
+++ packages/cli/tests/integration.rs
@@ -2,11 +2,23 @@ use assert_cmd::Command;
 use predicates::prelude::*;
 
 #[test]
-fn test_get_packages_cli() {
+#[ignore]
+fn get_packages() {
     let mut cmd = Command::cargo_bin("kittynode").unwrap();
-
     cmd.arg("get-packages")
         .assert()
         .success()
         .stdout(predicate::str::contains("Package: Ethereum"));
+}
+
+#[test]
+#[ignore]
+fn install_and_delete_a_package() {
+    let mut cmd = Command::cargo_bin("kittynode").unwrap();
+    cmd.arg("install-package")
+        .arg("Ethereum")
+        .assert()
+        .success();
+    cmd = Command::cargo_bin("kittynode").unwrap();
+    cmd.arg("delete-package").arg("Ethereum").assert().success();
 }
