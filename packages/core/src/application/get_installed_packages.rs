f0e98f11c87d2ec67e42a32e3ce5818a1b504ac6
dionysuz <13951458+dionysuzx@users.noreply.github.com>
2024-11-26 10:56:10 +0700

    get_installed_packages use case

---
+++ packages/core/src/application/get_installed_packages.rs
@@ -0,0 +1,32 @@
+use crate::{
+    domain::package::Package,
+    infra::docker::{find_container, get_docker_instance},
+    package::get_packages,
+};
+use eyre::{Context, Result};
+
+pub async fn get_installed_packages() -> Result<Vec<Package>> {
+    let docker = get_docker_instance()?;
+    let packages = get_packages().wrap_err("Failed to retrieve packages")?;
+
+    let mut installed = Vec::new();
+
+    // For each package, check if all containers exist
+    for package in packages.values() {
+        let mut all_containers_exist = true;
+
+        for container in &package.containers {
+            if find_container(&docker, &container.name).await?.is_empty() {
+                all_containers_exist = false;
+                break;
+            }
+        }
+
+        // If they do, add the package to the list
+        if all_containers_exist {
+            installed.push(package.clone());
+        }
+    }
+
+    Ok(installed)
+}
--- packages/core/src/application/mod.rs
+++ packages/core/src/application/mod.rs
@@ -1,5 +1,6 @@
 pub mod delete_kittynode;
 pub mod delete_package;
+pub mod get_installed_packages;
 pub mod get_system_info;
 pub mod init_kittynode;
 pub mod install_package;
