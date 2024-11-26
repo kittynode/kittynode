778ce19196c9f1405ed741826004f78536ae3bfe
dionysuz <13951458+dionysuzx@users.noreply.github.com>
2024-11-26 10:49:47 +0700

    install_package use case

--- packages/core/src/package.rs
+++ packages/core/src/package.rs
@@ -1,9 +1,5 @@
 use crate::domain::package::{Package, PackageDefinition};
-use crate::infra::docker::{
-    create_or_recreate_network, find_container, get_docker_instance, pull_and_start_container,
-    remove_container,
-};
-use crate::infra::file::generate_jwt_secret;
+use crate::infra::docker::{find_container, get_docker_instance, remove_container};
 use crate::manifests::ethereum::Ethereum;
 use eyre::{Context, Result};
 use std::collections::{HashMap, HashSet};
@@ -48,27 +44,6 @@ pub async fn get_installed_packages() -> Result<Vec<Package>> {
     }
 
     Ok(installed)
-}
-
-pub async fn install_package(name: &str) -> Result<()> {
-    let docker = get_docker_instance()?;
-
-    generate_jwt_secret().wrap_err("Failed to generate JWT secret")?;
-
-    let packages = get_packages().wrap_err("Failed to retrieve packages")?;
-    let package = packages
-        .get(name)
-        .ok_or_else(|| eyre::eyre!("Package '{}' not found", name))?;
-
-    let network_name = &package.network_name;
-    create_or_recreate_network(&docker, network_name).await?;
-
-    for container in &package.containers {
-        pull_and_start_container(&docker, container, network_name).await?;
-    }
-
-    info!("Package '{}' installed successfully.", name);
-    Ok(())
 }
 
 pub async fn delete_package(package_name: &str, include_images: bool) -> Result<()> {
