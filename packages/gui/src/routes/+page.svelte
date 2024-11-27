51aabbfadf4b532c2c053048d237016786235957
dionysuz <13951458+dionysuzx@users.noreply.github.com>
2024-11-27 23:51:42 +0900

    add comment

--- packages/gui/src/routes/+page.svelte
+++ packages/gui/src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 import Home from "./Home.svelte";
 </script>
-
+<!-- todo: is it possible to make a /home and thats the default route? or is it a bad idea -->
+<!-- basically thinking of the organization of package/ under routes -->
 <Home />
