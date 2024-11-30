3100906ce49006d4ffdbe20c6e24ec232285ee2a
dionysuz <13951458+dionysuzx@users.noreply.github.com>
2024-11-30 09:43:08 -0500

    chore(gui): navigation bar padding

--- packages/gui/src/routes/Navigation.svelte
+++ packages/gui/src/routes/Navigation.svelte
@@ -5,42 +5,34 @@ import { House, HeartPulse, Settings, KeyRound } from "lucide-svelte";
 
 <nav>
   <div class="flex h-5 items-center justify-between text-sm font-semibold">
-    <div class="flex-grow flex justify-center">
-      <a class="p-4" href="/" aria-current={$page.url.pathname === "/"}>
-        <House strokeWidth={$page.url.pathname === "/" ? 2.25 : 2} />
-      </a>
-    </div>
-    <div class="flex-grow flex justify-center">
-      <a
-        class="p-4"
-        href="/system-info"
-        aria-current={$page.url.pathname === "/system-info"}
-      >
-        <HeartPulse
-          strokeWidth={$page.url.pathname === "/system-info" ? 2.25 : 2}
-        />
-      </a>
-    </div>
-    <div class="flex-grow flex justify-center">
-      <a
-        class="p-4"
-        href="/key-manager"
-        aria-current={$page.url.pathname === "/key-manager"}
-      >
-        <KeyRound
-          strokeWidth={$page.url.pathname === "/key-manager" ? 2.25 : 2}
-        />
-      </a>
-    </div>
-    <div class="flex-grow flex justify-center">
-      <a
-        class="p-4"
-        href="/settings"
-        aria-current={$page.url.pathname === "/settings"}
-      >
-        <Settings strokeWidth={$page.url.pathname === "/settings" ? 2.25 : 2} />
-      </a>
-    </div>
+    <a
+      class="p-4 ps-0"
+      href="/"
+      aria-current={$page.url.pathname === "/"}
+    >
+      <House strokeWidth={$page.url.pathname === "/" ? 2.25 : 2} />
+    </a>
+    <a
+      class="p-4"
+      href="/system-info"
+      aria-current={$page.url.pathname === "/system-info"}
+    >
+      <HeartPulse strokeWidth={$page.url.pathname === "/system-info" ? 2.25 : 2} />
+    </a>
+    <a
+      class="p-4"
+      href="/key-manager"
+      aria-current={$page.url.pathname === "/key-manager"}
+    >
+      <KeyRound strokeWidth={$page.url.pathname === "/key-manager" ? 2.25 : 2} />
+    </a>
+    <a
+      class="p-4 pe-0"
+      href="/settings"
+      aria-current={$page.url.pathname === "/settings"}
+    >
+      <Settings strokeWidth={$page.url.pathname === "/settings" ? 2.25 : 2} />
+    </a>
   </div>
 </nav>
 
