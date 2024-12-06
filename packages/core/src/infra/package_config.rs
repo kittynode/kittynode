5847d68897c1676dba1c4983666234c55b99d28a
dionysuz <13951458+dionysuzx@users.noreply.github.com>
2024-12-06 12:34:30 -0500

    use toml

--- packages/core/src/infra/package_config.rs
+++ packages/core/src/infra/package_config.rs
@@ -1,7 +1,6 @@
 use crate::domain::package::PackageConfig;
 use crate::infra::file::kittynode_path;
 use eyre::Result;
-use serde_json;
 use std::{fs, path::PathBuf};
 
 pub struct PackageConfigStore;
@@ -12,8 +11,8 @@ impl PackageConfigStore {
         if !config_path.exists() {
             return Ok(PackageConfig::default());
         }
-        let json_str = fs::read_to_string(config_path)?;
-        let config = serde_json::from_str(&json_str)?;
+        let toml_str = fs::read_to_string(config_path)?;
+        let config = toml::from_str(&toml_str)?;
         Ok(config)
     }
 
@@ -22,8 +21,8 @@ impl PackageConfigStore {
         if let Some(parent) = config_path.parent() {
             fs::create_dir_all(parent)?;
         }
-        let json_str = serde_json::to_string_pretty(config)?;
-        fs::write(config_path, json_str)?;
+        let toml_str = toml::to_string_pretty(config)?;
+        fs::write(config_path, toml_str)?;
         Ok(())
     }
 
@@ -31,7 +30,7 @@ impl PackageConfigStore {
         let mut path = kittynode_path()?;
         path.push("packages");
         path.push(package_name);
-        path.push("config.json");
+        path.push("config.toml");
         Ok(path)
     }
 }
